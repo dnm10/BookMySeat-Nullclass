38656c02946436047243ddfbc9de30a366a6d842
dnm10 <deeptinm10@gmail.com>
2025-02-23 09:52:42 +0530

    Done with Ticket Email Confirmation

--- bookmyseat/settings.py
+++ bookmyseat/settings.py
@@ -58,8 +58,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'your_email@gmail.com'
-EMAIL_HOST_PASSWORD = 'your_app_password'
+EMAIL_HOST_USER = 'deeptinm10@gmail.com'
+EMAIL_HOST_PASSWORD = 'kpyg jkak qziv clcs'
 
 
 MEDIA_URL = '/media/'
--- movies/views.py
+++ movies/views.py
@@ -25,49 +25,63 @@ def theater_list(request, movie_id):
 def book_seats(request, theater_id):
     theaters = get_object_or_404(Theater, id=theater_id)
     seats = Seat.objects.filter(theater=theaters)
+
     if request.method == 'POST':
-        selected_Seats = request.POST.getlist('seats')
+        selected_seats = request.POST.getlist('seats')
         error_seats = []
         booked_seats = []
 
-        if not selected_Seats:
-            return render(request, "movies/seat_selection.html", {'theaters': theaters, "seats": seats, 'error': "No seat selected"})
+        if not selected_seats:
+            return render(request, "movies/seat_selection.html", {
+                'theaters': theaters,
+                "seats": seats,
+                'error': "No seat selected"
+            })
 
-        for seat_id in selected_Seats:
+        for seat_id in selected_seats:
             seat = get_object_or_404(Seat, id=seat_id, theater=theaters)
+
             if seat.is_booked:
                 error_seats.append(seat.seat_number)
                 continue
+
             try:
-                
-                Booking = Booking.objects.create(
+                booking = Booking.objects.create(
                     user=request.user,
                     seat=seat,
                     movie=theaters.movie,
                     theater=theaters
                 )
+
                 seat.is_booked = True
                 seat.save()
 
-                booked_seats.append(Booking)
+                booked_seats.append(booking)
 
             except IntegrityError:
                 error_seats.append(seat.seat_number)
 
         if error_seats:
             error_message = f"The following seats are already booked: {', '.join(error_seats)}"
-            return render(request, 'movies/seat_selection.html', {'theaters': theaters, "seats": seats, 'error': error_message})
+            return render(request, 'movies/seat_selection.html', {
+                'theaters': theaters,
+                "seats": seats,
+                'error': error_message
+            })
 
+        # Send confirmation email
         send_booking_confirmation_email(booked_seats, request.user)
 
         messages.success(request, 'Your seats have been successfully booked! Check your email for details.')
-
         return redirect('profile')
 
     return render(request, 'movies/seat_selection.html', {'theaters': theaters, "seats": seats})
 
 
 def send_booking_confirmation_email(bookings, user):
+    if not bookings:
+        return
+
     subject = 'Booking Confirmation - Your Movie Tickets'
     seat_numbers = ', '.join([booking.seat.seat_number for booking in bookings])
     theater_name = bookings[0].theater.name
@@ -90,12 +104,14 @@ Regards,
 Movie Ticket Booking Team
     """
 
-    recipient_email = user.email
+    # recipient_email = user.email
 
     send_mail(
         subject,
         message,
         settings.EMAIL_HOST_USER,
-        [recipient_email],
+        ['mahajandeepti830@gmail.com'],
         fail_silently=False,
     )
+
+
